a91b3dec6b20c3ec3e2b25ef71e8b49a2e92ed64
Cássia Queiroz <cassiarsqueiroz@gmail.com>
2023-11-18 23:58:27 -0300

    Modificações na seção de Porfólio e na de Contato

--- forms/contact.php
+++ forms/contact.php
@@ -1,8 +1,7 @@
 <?php
 
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'cassiarsqueiroz@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -18,16 +17,7 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
+  
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
