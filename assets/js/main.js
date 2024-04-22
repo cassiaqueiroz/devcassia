235e341e5c863c2e02eab77251a10690b251d86a
cassiaqueiroz <cassiarsqueiroz@gmail.com>
2024-04-22 14:39:48 -0300

    mudei toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,8 +101,8 @@
       if (body.classList.contains('mobile-nav-active')) {
         body.classList.remove('mobile-nav-active')
         let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        navbarToggle.classList.toggle('bi-toggle-on')
+        navbarToggle.classList.toggle('bi-toggle-off')
       }
       scrollto(this.hash)
     }
